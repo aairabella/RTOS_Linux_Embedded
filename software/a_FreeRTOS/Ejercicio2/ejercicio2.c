cbda77c64ec4230955e08f5e7b0731e56a170a6e
Andres Miguel Airabella <tuk@satellogic.com>
2018-03-30 01:06:35 -0300

    cambio de prioridades en las tasks

--- software/a_FreeRTOS/Ejercicio2/ejercicio2.c
+++ software/a_FreeRTOS/Ejercicio2/ejercicio2.c
@@ -5,10 +5,10 @@
 #include "xil_printf.h"
 #include "xparameters.h"
 
-#define TIMER_ID	1
-#define DELAY_10_SECONDS	10000UL
-#define DELAY_1_SECOND		1000UL
-#define TIMER_CHECK_THRESHOLD	9
+#define TIMER_ID    1
+#define DELAY_10_SECONDS    10000UL
+#define DELAY_1_SECOND      1000UL
+#define TIMER_CHECK_THRESHOLD   9
 
 
 static void prvTxTask( void *pvParameters );
@@ -24,95 +24,97 @@ long RxtaskCntr = 0;
 
 int main( void )
 {
-	const TickType_t x10seconds = pdMS_TO_TICKS( DELAY_10_SECONDS );
+    const TickType_t x10seconds = pdMS_TO_TICKS( DELAY_10_SECONDS );
 
-	xil_printf( "\n\rHello from Freertos example main\r\n" );
+    xil_printf( "\n\rHello from Freertos example main\r\n" );
 
-	xTaskCreate( 	prvTxTask,
-					( const char * ) "Tx",
-					configMINIMAL_STACK_SIZE,
-					NULL,
-					tskIDLE_PRIORITY,
-					&xTxTask );
+    xTaskCreate( prvTxTask,
+                 ( const char * ) "Tx",
+                 configMINIMAL_STACK_SIZE,
+                 NULL,
+                 tskIDLE_PRIORITY+1,
+                 &xTxTask );
 
-	// Esta tarea tiene mayor prioridad.
-	xTaskCreate( prvRxTask,
-				 ( const char * ) "GB",
-				 configMINIMAL_STACK_SIZE,
-				 NULL,
-				 tskIDLE_PRIORITY + 1,
-				 &xRxTask );
-
-
-	// Solo puede almacenarse un elemento en esta cola.
-	xQueue = xQueueCreate( 	1,
-							sizeof( HWstring ) );
+    // Esta tarea tiene mayor prioridad.
+    xTaskCreate ( prvRxTask,
+                 ( const char * ) "GB",
+                 configMINIMAL_STACK_SIZE,
+                 NULL,
+                 tskIDLE_PRIORITY,
+                 &xRxTask );
 
 
-	configASSERT( xQueue );
+    // Solo puede almacenarse un elemento en esta cola.
+    xQueue = xQueueCreate(  1,
+                            sizeof( HWstring ) );
 
-	xTimer = xTimerCreate( (const char *) "Timer",
-							x10seconds,
-							pdFALSE,
-							(void *) TIMER_ID,
-							vTimerCallback);
 
-	configASSERT( xTimer );
+    configASSERT( xQueue );
 
-	xTimerStart( xTimer, 0 );
+    xTimer = xTimerCreate( (const char *) "Timer",
+                            x10seconds,
+                            pdFALSE,
+                            (void *) TIMER_ID,
+                            vTimerCallback);
 
-	vTaskStartScheduler();
+    configASSERT( xTimer );
 
-	for( ;; );
+    xTimerStart( xTimer, 0 );
+
+    vTaskStartScheduler();
+
+    for( ;; );
 }
 
 static void prvTxTask( void *pvParameters )
 {
 const TickType_t x1second = pdMS_TO_TICKS( DELAY_1_SECOND );
 
-	for( ;; )
-	{
-		vTaskDelay( x1second );
+    for( ;; )
+    {
+        xil_printf("Task Tx executed\r\n");
+        vTaskDelay( x1second );
 
-		xQueueSend( xQueue,
-					HWstring,
-					0UL );
-	}
+        xQueueSend( xQueue,
+                    HWstring,
+                    0UL );
+    }
 }
 
 static void prvRxTask( void *pvParameters )
 {
 char Recdstring[15] = "";
 
-	for( ;; )
-	{
-		xQueueReceive( 	xQueue,
-						Recdstring,
-						portMAX_DELAY );
+    for( ;; )
+    {
+        xil_printf("Task Rx executed\r\n");
+        xQueueReceive(  xQueue,
+                        Recdstring,
+                        portMAX_DELAY );
 
-		xil_printf( "Rx task received string from Tx task: %s\r\n", Recdstring );
-		RxtaskCntr++;
-	}
+        xil_printf( "Rx task received string from Tx task: %s\r\n", Recdstring );
+        RxtaskCntr++;
+    }
 }
 
 static void vTimerCallback( TimerHandle_t pxTimer )
 {
-	long lTimerId;
-	configASSERT( pxTimer );
+    long lTimerId;
+    configASSERT( pxTimer );
 
-	lTimerId = ( long ) pvTimerGetTimerID( pxTimer );
+    lTimerId = ( long ) pvTimerGetTimerID( pxTimer );
 
-	if (lTimerId != TIMER_ID) {
-		xil_printf("FreeRTOS Hello World Example FAILED");
-	}
+    if (lTimerId != TIMER_ID) {
+        xil_printf("FreeRTOS Hello World Example FAILED");
+    }
 
-	if (RxtaskCntr >= TIMER_CHECK_THRESHOLD) {
-		xil_printf("FreeRTOS Hello World Example PASSED");
-	} else {
-		xil_printf("FreeRTOS Hello World Example FAILED");
-	}
+    if (RxtaskCntr >= TIMER_CHECK_THRESHOLD) {
+        xil_printf("FreeRTOS Hello World Example PASSED");
+    } else {
+        xil_printf("FreeRTOS Hello World Example FAILED");
+    }
 
-	vTaskDelete( xRxTask );
-	vTaskDelete( xTxTask );
+    vTaskDelete( xRxTask );
+    vTaskDelete( xTxTask );
 }
 
